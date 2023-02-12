378a3af6171410b5b78f9914a2ef1bc83b2e0a85
amirandakhs <amirandakhs1375@gmail.com>
2023-02-12 17:22:57 +0800

    Adding the get request for the map API key

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,11 +1,18 @@
-import express from 'express';
+import express, { json } from 'express';
+import 'dotenv/config';
 import { Location } from '../const/const.js';
 
 const app = express();
 app.use(express.json());
 
 app.get("/StoreLocation", (req, res) => {
-    res.send(JSON.stringify(Location));
+    //If we this is not safe we can use the .env process.env.LOCATION
+    res.status(200).send(JSON.stringify(Location));
+});
+
+app.get("/Map", (req, res) => {
+    console.log(process.env.MAP_API_KEY);
+    res.status(200).send(process.env.MAP_API_KEY);
 });
 
 app.listen(8000, ()=>{
