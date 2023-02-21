37ae2949dcc4fe2c07c69c858a7dbd7dfb406dcd
tara <tara.damavandi94@gmail.com>
2023-02-22 00:00:20 +0330

    google map using iframe

--- src/components/Footers/CardsFooter.js
+++ src/components/Footers/CardsFooter.js
@@ -31,12 +31,14 @@ import {
   Col,
   UncontrolledTooltip,
 } from "reactstrap";
-import MapSection from "../map/Map";
+
 const location = {
   address: "27 Century Rd, Malaga WA 6090",
   lat: -31.857093637082734,
   lng: 115.89644365231285,
 };
+const position = [-31.857093637082734, 115.89644365231285];
+
 class CardsFooter extends React.Component {
   render() {
     return (
@@ -96,7 +98,18 @@ class CardsFooter extends React.Component {
                     <h4>COMPANY NAME</h4>
                     <p>explain a little about the company</p>
                   </div>
-                  <MapSection location={location} zoomLevel={10} />{" "}
+                  <div>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3388.820882842161!2d115.8942550147842!3d-31.85708902489312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDUxJzI1LjUiUyAxMTXCsDUzJzQ3LjIiRQ!5e0!3m2!1sen!2snl!4v1677009918630!5m2!1sen!2snl"
+                      width="400"
+                      height="300"
+                      style="border:0;"
+                      allowfullscreen=""
+                      loading="lazy"
+                      referrerpolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
+
                   <div className="col-md-3 col-sm-6 ml-5">
                     <h4>CONTACT</h4>
                     <ul className="unstyled"></ul>
